66b06b529a72e131a5a571af2349c6633f7abb0a
Viktar Kazlou <KazlouV@iba>
2017-09-18 16:34:09 +0300

    hardcode variant

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var n = str.length;
+    for(var i = 0 ; i < n ; i++){
+        str = str.replace("()", "");
+        str = str.replace("{}", "");
+        str = str.replace("[]", "");
+        str = str.replace("||", "");
+        str = str.replace("12", "");
+        str = str.replace("34", "");
+        str = str.replace("56", "");
+        str = str.replace("77", "");
+        str = str.replace("88", "");
+    }
+    if(str.length) return false;
+    else return true;  
 }
